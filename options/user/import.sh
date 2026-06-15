cfa4b6655fcdaf071d3a282cfb6a3e6e68158e5b
Jordi Barreda <133088258+gordo1998@users.noreply.github.com>
2026-06-15 16:39:47 +0200

    Refactor user import script to use unified library path

--- options/user/import.sh
+++ options/user/import.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 UI_DIR_SOURCE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-UI_LIB_PAR_EXT="$(cd "$UI_DIR_SOURCE/../../lib/parser" && pwd)"
-UI_LIB_MOD="$(cd "$UI_DIR_SOURCE/../../lib/mode" && pwd)"
+LIB_DIR="$(cd "$UI_DIR_SOURCE/../../lib" && pwd)"
 
-source "$UI_LIB_PAR_EXT/detect_extensions/detect_extensions.sh"
-source "$UI_LIB_MOD/lib_mode_det_mode.sh"
+source "$LIB_DIR/parser/detect_extensions/detect_extensions.sh"
+source "$LIB_DIR/mode/lib_mode_det_mode.sh"
+source "$LIB_DIR/user/execution.sh"
+source "$LIB_DIR/user/validation.sh"
 
-
-UI_PARSED=$(lpd_det_ext "$@")
-UI_DIR=$(lm_det_mode "$@")
-
-#Un while tiene que haber si o si. Antes del while se llama a una funcion que parsea la info. 
-
+#EN BASE A LA LISTA PARSEADA DE UI_PARSED EJECUTA LAS INSTRUCCIONES PARA LA CREACIÓN DE USUARIOS
 ui_import_users(){
+	#ESTA VARIABLE SE ENCARGA DE RECOGER LA LISTA PARSEADA.
+	local ui_parsed=$(lpd_det_ext "$@")
+	#ESTA DETECTA EL MODO DEL ENTORNO (LABORATORIO O NORMAL PARA CREAR EL PATH RAIZ)
+	local ui_dir=$(lm_det_mode "$@")
+
 	while IFS="|" read -r user path_user;do
+		local dir_complete="$ui_dir/$path_user"
 		echo "Creando usuario $user..."
-		sudo useradd -m -d "$UI_DIR/$path_user" "$user"
-		([[ $? == 0 ]] && echo "Usuario $user creado correctamente") || (echo "Usuario $user no se ha podido crear o ya existe" && exit 1)
+		exec_comm "$user" "$dir_complete"
 	done <<< "$UI_PARSED"
 }
